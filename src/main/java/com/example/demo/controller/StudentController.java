7e248176c16695079a2bbbdc0e74b05e78d5b85e
bala-amypo <bala@amypo.in>
2025-12-17 06:46:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.*;
+import org.springframework.web.bind.annotation.*;
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.service.StudentInterface;
 
--- src/main/java/com/example/demo/repository/Repository.java
+++ src/main/java/com/example/demo/repository/Repository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import co.exaple.demo.entity.StudentEntity;
+import com.example.demo.entity.StudentEntity;
 
 public interface Repository extends JpaRepository<StudentEntity,Integer>{
 
