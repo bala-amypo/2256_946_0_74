bd4965284417e5e244be940aa71c7a8575c88c2c
bala-amypo <bala@amypo.in>
2025-12-17 06:31:21 +0000

    User commit

--- src/main/java/com/example/demo/service/servicelogic/StudentService.java
+++ src/main/java/com/example/demo/service/servicelogic/StudentService.java
@@ -6,3 +6,18 @@ import com.example.demo.entity.StudentEntity;
 import com.example.demo.repository.Repository;
 import com.example.demo.service.StudentInterface;
 
+@Service
+public class StudentService implements StudentInterface{
+
+    @Autowired
+    Repository repo;
+
+    @Override
+    public StudentEntity createdata(StudentEntity stu){
+        return repo.save(stu);
+    }
+    @Override
+    public List<StudentEntity> dataList(){
+        return repo.findAll();
+    }
+}
