c4c9821d3bc8fd4f7fe40243e1fde5889f95aaae
bala-amypo <bala@amypo.in>
2025-12-17 06:01:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -8,5 +8,17 @@ import com.example.demo.StudentInterface;
 
 @RestController
 public class StudentController{
-    
+
+    @Autowired
+    StudentInterface ser;
+
+    @PostMapping("/newdata")
+    public StudentEntity createdata(StudentEntity stu){
+        ser.createdata(stu);
+    }
+
+    @GetMapping("/getdata")
+    public List<StudentEntity> dataList(){
+        return ser.dataList();
+    }
 }
